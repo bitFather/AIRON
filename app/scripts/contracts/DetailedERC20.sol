ad594585222e66aba266e77c5be5e486b9e906ce
Анатолий Рюмкин <rumsterrt@gmail.com>
2018-01-30 17:49:36 +0700

    Updated smart contract, fixed erc20

--- app/scripts/contracts/DetailedERC20.sol
+++ app/scripts/contracts/DetailedERC20.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.15;
 contract ERC20 {
     function totalSupply() external constant returns (uint256 _totalSupply);
     function balanceOf(address _owner) external constant returns (uint256 balance);
-    function userTransfer(address _to, uint256 _value) external returns (bool success);
-    function userTransferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-    function userApprove(address _spender, uint256 _old, uint256 _new) external returns (bool success);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _old, uint256 _new) external returns (bool success);
     function allowance(address _owner, address _spender) external constant returns (uint256 remaining);
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -69,7 +69,7 @@ contract DetailedERC20 is ERC20 {
         _;
     }
 
-    function userTransfer(address _to, uint256 _value) onlyPayloadSize(64) external returns (bool success) {
+    function transfer(address _to, uint256 _value) onlyPayloadSize(64) external returns (bool success) {
         if (accounts[msg.sender] >= _value) {
             accounts[msg.sender] = accounts[msg.sender].safeSub(_value);
             accounts[_to] = accounts[_to].safeAdd(_value);
@@ -80,7 +80,7 @@ contract DetailedERC20 is ERC20 {
         }
     }
 
-    function userTransferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(64) external returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(64) external returns (bool success) {
         if ((accounts[_from] >= _value) && (allowed[_from][msg.sender] >= _value)) {
             accounts[_from] = accounts[_from].safeSub(_value);
             allowed[_from][msg.sender] = allowed[_from][msg.sender].safeSub(_value);
@@ -92,7 +92,7 @@ contract DetailedERC20 is ERC20 {
         }
     }
 
-    function userApprove(address _spender, uint256 _old, uint256 _new) onlyPayloadSize(64) external returns (bool success) {
+    function approve(address _spender, uint256 _old, uint256 _new) onlyPayloadSize(64) external returns (bool success) {
         if (_old == allowed[msg.sender][_spender]) {
             allowed[msg.sender][_spender] = _new;
             Approval(msg.sender, _spender, _new);
